75fba7c7e28115eb7731d2173a78d14d7175e832
Jeffrey R. Carter <16976909+jrcarter@users.noreply.github.com>
2025-06-20 10:23:53 +0000

    Use SPDX license format

--- ada_gui.adb
+++ ada_gui.adb
@@ -1,9 +1,12 @@
 -- An Ada-oriented GUI library
 -- Implementation derived from Gnoga
 --
--- Copyright (C) 2025 by PragmAda Software Engineering
+-- Copyright (C) by PragmAda Software Engineering
 --
--- Released under the terms of the 3-Clause BSD License. See https://opensource.org/licenses/BSD-3-Clause
+-- SPDX-License-Identifier: BSD-3-Clause
+-- See https://spdx.org/licenses/
+-- If you find this software useful, please let me know, either through
+-- github.com/jrcarter or directly to pragmada@pragmada.x10hosting.com
 
 with Ada.Containers.Vectors;
 with Ada.Exceptions;
--- ada_gui.ads
+++ ada_gui.ads
@@ -1,8 +1,11 @@
 -- An Ada-oriented GUI library specification. The included sample implementation uses a browser as its platform
 --
--- Copyright (C) 2025 by PragmAda Software Engineering
+-- Copyright (C) by PragmAda Software Engineering
 --
--- Released under the terms of the 3-Clause BSD License. See https://opensource.org/licenses/BSD-3-Clause
+-- SPDX-License-Identifier: BSD-3-Clause
+-- See https://spdx.org/licenses/
+-- If you find this software useful, please let me know, either through
+-- github.com/jrcarter or directly to pragmada@pragmada.x10hosting.com
 
 with Ada.Strings.Unbounded;
 
